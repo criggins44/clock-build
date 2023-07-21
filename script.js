8f782222d253a27b7805c734c84c6c0900ed819e
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-07-21 16:25:26 -0400

    added JS to retrieve current time data, updated the CSS styling to a darker theme

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Clock Build
+
+
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+const clock = setInterval(function time(){
+	const currentDate = new Date();
+	let hr = currentDate.getHours() % 12 || 12;
+	let min = currentDate.getMinutes();
+	let sec = currentDate.getSeconds();
+
+	hr = hr.toString().padStart(2,"0");
+	min = min.toString().padStart(2,"0");
+	sec = sec.toString().padStart(2,"0");
+
+	const timeString = '${hr}:${min}:${sec}'
+
+	hours.innerHTML = hr;
+	minutes.innerHTML = min;
+	seconds.innerHTML = sec;
+},1000);
+
